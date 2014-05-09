e3346c9c0279d55f9b9b23bff945c83e62bfe8bb
Jun-ya HASEBA <7pairs@gmail.com>
2014-05-09 23:06:23 +0900

    Fix a bug in test_get_xml_01() and test_get_xml_02() #3

--- test/test_convert.py
+++ test/test_convert.py
@@ -5,11 +5,11 @@ from nose.tools import *
 from wp2mt import convert
 
 
-def test_get_xml():
+def test_get_xml_01():
     """
     引数に有効なファイルパスを指定したとき、そのパスのXMLの内容を文字列として返すことを確認する。
     """
-    xml = convert.get_xml('./test.xml')
+    xml = convert.get_xml('./test/test.xml')
     actual = ('1つ目の記事' in xml)
     assert_equal(True, actual)
 
@@ -18,6 +18,6 @@ def test_get_xml_02():
     """
     引数に無効なファイルパスを指定したとき、空文字列を返すことを確認する。
     """
-    xml = convert.get_xml('./not_exists.xml')
+    xml = convert.get_xml('./test/not_exists.xml')
     assert_equal('', xml)
 
